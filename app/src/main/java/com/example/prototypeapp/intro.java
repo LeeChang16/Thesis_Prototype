3a22eecfa642f32e7ea6a60a33526099cb517fe4
LeeChang16 <chandleerey54@gmail.com>
2023-10-11 13:56:34 -0700

    fifth commit

--- app/src/main/java/com/example/prototypeapp/intro.java
+++ app/src/main/java/com/example/prototypeapp/intro.java
@@ -2,7 +2,9 @@ package com.example.prototypeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class intro extends AppCompatActivity {
@@ -15,6 +17,21 @@ Button login,signup;
         login = (Button) findViewById(R.id.login);
         signup = (Button) findViewById(R.id.signup);
 
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(intro.this,login.class);
+            startActivity(intent);
+        }
+    });
+
+    signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(intro.this,signup.class);
+            startActivity(intent);
+        }
+    });
 
     }
 }
